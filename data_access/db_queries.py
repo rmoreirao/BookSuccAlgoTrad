79bf2d77f4b4622c35d3324879a089b67454e5ca
Rodrigo Moreirao <rodrigo.goes.moreirao@ing.com>
2019-05-31 17:24:46 +0200

    Returning

--- data_access/db_queries.py
+++ data_access/db_queries.py
@@ -3,7 +3,7 @@ import MySQLdb as mdb
 
 from data_access import db_helper
 
-class get_daily_adj_close():
+def get_daily_adj_close():
     COL_PRICE_DATE = "price_date"
     COL_ADJ_CLOSE_PRICE = "adj_close_price"
 
@@ -16,3 +16,6 @@ class get_daily_adj_close():
         WHERE sym.ticker = {}
         ORDER BY dp.price_date ASC;""".format(self.COL_PRICE_DATE,self.COL_ADJ_CLOSE_PRICE, ticker)
         return pd.read_sql_query(sql, con=con, index_col='price_date')
+
+if __name__ == "__main__":
+    print(get_daily_adj_close('AAPL'))
